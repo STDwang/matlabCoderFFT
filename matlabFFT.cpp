722ba6e885c69d6269588441dc3573d2cff209c3
wang-shuaipeng0123 <1946262980@qq.com>
2022-04-04 20:36:09 +0800

    new

--- matlabFFT.cpp
+++ matlabFFT.cpp
@@ -1,20 +1,53 @@
-﻿// matlabFFT.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
-//
-
+﻿#include "rt_nonfinite.h"
+#include "test.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include "rtwtypes.h"
+#include "test_types.h"
+#include "test_terminate.h"
+#include "test_emxAPI.h"
+#include "test_initialize.h"
 #include <iostream>
+using namespace std;
 
-int main()
+static emxArray_real_T *argInit_1xUnbounded_real_T()
 {
-    std::cout << "Hello World!\n";
+	static int iv0[2] = { 1, 2 };
+	emxArray_real_T *result = emxCreateND_real_T(2, iv0);
+
+	for (int idx1 = 0; idx1 < result->size[1]; idx1++) {
+		result->data[result->size[0] * idx1] = 0.0;
+	}
+
+	return result;
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
+int main(int, const char * const[])
+{
+	test_initialize();
 
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
+	emxArray_creal_T *fp;
+	emxArray_real_T *Id1;
+	emxArray_real_T *Qd1;
+
+	emxInitArray_creal_T(&fp, 2);
+	int iv0[2] = { 1, 60 };
+	emxArray_real_T *Id1 = emxCreateND_real_T(2, iv0);
+	emxArray_real_T *Qd1 = emxCreateND_real_T(2, iv0);
+
+	//假数据
+	for (int idx1 = 0; idx1 < iv0[1]; idx1++) {
+		Id1->data[Id1->size[0] * idx1] = 0.0;
+		Qd1->data[Qd1->size[0] * idx1] = 0.0;
+	}
+
+	test(Id1, Qd1, fp);
+	cout << fp->data[0].re << "+" << fp->data[0].im << "i" << endl;
+
+	emxDestroyArray_creal_T(fp);
+	emxDestroyArray_real_T(Qd1);
+	emxDestroyArray_real_T(Id1);
+
+	test_terminate();
+	return 0;
+}
